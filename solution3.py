5344271f1b9839d36f74126496d54747abafa9a0
jssmith79 <i.am.hef@gmail.com>
2023-06-25 10:19:26 -0700

    completed activity 1-4

--- solution3.py
+++ solution3.py
@@ -1,1 +1,17 @@
 # Write code for algorithm 3 below
+
+def fib(n):
+    if n<= 0:
+        print("invalid input")
+    elif n ==1:
+        return 0
+    elif n == 2:
+        return 1
+    else: 
+        return fib(n-1) + fib(n-2)
+print("2nd fib number:")
+print(fib(2))
+print("4th fib number:")
+print(fib(4))
+print("8th fib number:")
+print(fib(8))
--- solution4.py
+++ solution4.py
@@ -1,1 +1,12 @@
 # Write code for algorithm 4 below
+
+def exp(a,b):
+    if b < 1:
+        print("invalid input")
+    elif b == 1:
+        return a
+    else:
+        return a * exp(a,b=1)
+
+print ("3^1:")
+print(exp(3,1))
